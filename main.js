13fe24925adfea13fd6faccdd2bb5c5446e45c7a
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-02-01 14:39:07 +0000

    Adding Paddle Movement

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+function Board() {
+    let player = document.querySelector('#player-paddle')
+    //let enemy = document.querySelector('#enemy-paddle')
+
+    //console.log(player)
+    let board = document.querySelector('#container')
+
+    board.addEventListener('mousemove', mouseHandler)
+
+    console.log(player)
+}
+
+let game = new Board()
+
+function mouseHandler(e) {
+    console.log(e.clientY + 'px')
+}
 
