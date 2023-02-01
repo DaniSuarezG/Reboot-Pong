de93f3b582aa1f10be47855325a0daac4cae4caf
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-02-01 17:04:02 +0000

    Refactoring

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ function Board() {
     function mouseHandler(e) {
         
         //let variable = parseInt(player.style.left.slice(0, player.style.left.length - 2)) + 250
-        let wallRight = e.clientX - (250 / 2)
-        let wallLeft = e.clientX + (250 / 2)
+        let playerRight = e.clientX + (250 / 2)
+        let playerLeft = e.clientX - (250 / 2)
 
-        if (wallRight <= 470 && wallLeft >= 470) {
+        if (true) {
             player.style.left = (e.clientX - (window.innerWidth / 2) + (470 / 2) - (250 / 2)) + 'px'
-            wallRight = e.clientX + (250 / 2)
-            wallLeft = e.clientX - (250 / 2)
+            playerRight = e.clientX + (250 / 2)
+            playerLeft = e.clientX - (250 / 2)
             
         }
         //console.log(wallLeft)
