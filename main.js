314ff73db4f93c9e78a75b0b1e2f6fc33b007afd
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-02-01 16:40:56 +0000

    Added Player Paddle Movement (Revision)

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 function Board() {
     let player = document.querySelector('#player-paddle')
+    player.style.left = '120px'
     //let enemy = document.querySelector('#enemy-paddle')
 
     //console.log(player)
     let board = document.querySelector('#container')
 
-    board.addEventListener('mousemove', mouseHandler)
+    board.onmousemove = mouseHandler
+    //board.addEventListener('mousemove', mouseHandler)
 
-    console.log(player)
+    //console.log((window.innerWidth / 2) - (board.clientLeft / 2))
+
+
+    function mouseHandler(e) {
+        
+        //let variable = parseInt(player.style.left.slice(0, player.style.left.length - 2)) + 250
+        let wallRight = e.clientX - (250 / 2)
+        let wallLeft = e.clientX + (250 / 2)
+
+        if (wallRight <= 470 && wallLeft >= 470) {
+            player.style.left = (e.clientX - (window.innerWidth / 2) + (470 / 2) - (250 / 2)) + 'px'
+            wallRight = e.clientX + (250 / 2)
+            wallLeft = e.clientX - (250 / 2)
+            
+        }
+        //console.log(wallLeft)
+        //console.log("MOUSE: " + e.clientX)
+        //console.log("PADDLE: " + parseInt(player.style.left.slice(0, player.style.left.length - 2)))
+    }
 }
 
 let game = new Board()
-
-function mouseHandler(e) {
-    console.log(e.clientY + 'px')
-}
 
