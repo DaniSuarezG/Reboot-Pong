cc9c620d196f33b1776c67616ddb91ba175ce695
isabelgm23 <isabelgm.trabajo@gmail.com>
2023-02-02 15:30:34 +0000

    Refactoring and debugging player bar movement

--- main.js
+++ main.js
@@ -38,45 +38,83 @@ function Board() {
 function Game(){
     this.board  = new Board()
     this.player = new Paddle()
-    this.enemy  = new Paddle()
-    
-    this.world =  document.querySelector('body')
+    // this.enemy  = new Paddle()
+    let self = this
 
-    let playerHtml = this.player.innerHTML
-    let enemyHtml = this.enemy.innerHTML
-    this.board.html.appendChild(this.player.html)
-    this.world.appendChild(this.board.html)
+    this.setUpBoard = function () {
+        let world   =  document.querySelector('body')
+
+        this.board.createBoard()
+        this.player.createPaddle('player', this.board.width)
+
+        //this.player.html.style.left = ((this.board.width / 2) - (this.player.width / 2)) + 'px'
+
+        this.board.html.appendChild(this.player.html)
+        //this.board.html.onmousemove = this.mouseHandler 
+        this.board.html.addEventListener('mousemove', function(e) {
+            self.mouseHandler(e)
+        })
+
+        
+        //this.board.html.appendChild(this.enemy.html)
+
+        world.appendChild(this.board.html)
+    }
+    
+
+
+    this.mouseHandler = function (e) {
+        let playerRight = e.clientX + (this.player.width / 2)
+        let playerLeft  = e.clientX - (this.player.width / 2)
+
+        let borderRight = (window.innerWidth / 2) + (this.board.width / 2) 
+        let borderLeft  = (window.innerWidth / 2) - (this.board.width / 2) 
+
+        if (playerRight < borderRight && playerLeft > borderLeft) {
+            this.player.html.style.left = (e.clientX - (window.innerWidth / 2) + (this.board.width / 2) - (this.player.width / 2)) + 'px'
+
+            /*
+            playerRight = e.clientX + (250 / 2)
+            playerLeft  = e.clientX - (250 / 2)
+            */
+        }
+    }
 }
 
 //clase board
 function Board(){
-    this.createHTML = function (){
-        this.html = document.createElement('div')
-        this.html.id = 'board'
-    }
-    this.createHTML()
     this.height = 700
-    this.html.style.height = this.height + 'px'   
-    this.width = this.height * this.ratio
-    this.html.style.width = this.width + 'px' 
+    this.ratio  = 4/7
+    this.width  = this.height * this.ratio
+    this.html   = null
+
+    this.createBoard = function (){
+        this.html    = document.createElement('div')
+        this.html.id = 'board'
+        this.html.style.height = this.height + 'px'    
+        this.html.style.width  = this.width + 'px' 
+    }
 }
-Board.prototype.ratio = 4/7
 
 //clase paddle con dos objetos
 function Paddle(){
-    
-    this.createHTML = function(){
-        this.html = document.createElement('div')
-        this.html.id = 'player-paddle'
-        this.html.classList.add('paddle')
-    }
-    this.createHTML()
     this.height = 30
-    this.html.style.height = this.height + 'px'
-    this.width = 250
-    this.html.style.width = this.width + 'px'
+    this.width  = 250
+    this.html   = null
+
+    this.createPaddle = function(classP, width){
+        this.html     = document.createElement('div')
+        this.html.id  = `${classP}-paddle`
+        this.html.classList.add('paddle')
+        this.html.style.height = this.height + 'px'
+        this.html.style.width  = this.width + 'px'
+        this.html.style.left = ((width / 2) - (this.width / 2)) + 'px'
+
+    }
+
 }
 //clase para la bola
 
 let game = new Game()
+game.setUpBoard()
 console.log(game)
