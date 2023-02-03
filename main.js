880b4a23a76653b6db396c042e20c75c0aceceb1
isabelgm23 <isabelgm.trabajo@gmail.com>
2023-02-03 16:59:11 +0000

    Working on Paddle Bouncing

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function Game(){
         let world   =  document.querySelector('body')
 
         this.board.createBoard()
-        this.player.createPaddle('player', this.board.width)
+        this.player.createPaddle('player', this.board.width, this.board.height)
 
         //this.player.html.style.left = ((this.board.width / 2) - (this.player.width / 2)) + 'px'
 
@@ -76,8 +76,9 @@ function Game(){
         let borderLeft  = (window.innerWidth / 2) - (this.board.width / 2) 
 
         if (playerRight < borderRight && playerLeft > borderLeft) {
-            this.player.html.style.left = 
-            (e.clientX - (window.innerWidth / 2) + (this.board.width / 2) - (this.player.width / 2)) + 'px'
+            this.player.left = (e.clientX - (window.innerWidth / 2) + (this.board.width / 2) - (this.player.width / 2))
+           // this.player.updateMove((e.clientX - (window.innerWidth / 2) + (this.board.width / 2) - (this.player.width / 2)) + 'px')
+            this.player.html.style.left = this.player.left + 'px'
 
             /*
             playerRight = e.clientX + (250 / 2)
@@ -88,9 +89,17 @@ function Game(){
 
     this.bounceHandler = function () {
         let borderRight = this.board.width
-        let borderLeft = 0
-        let borderUp = 0
-        let borderDown = this.board.height
+        let borderLeft  = 0
+        let borderUp    = 0
+        let borderDown  = this.board.height
+
+        let paddleRight = this.player.left + this.player.width
+        let paddleLeft  = this.player.left
+        let paddleUp    = this.player.top
+        let paddleDown  = this.player.top + this.player.height
+
+       //console.log("paddle: ", paddleDown, paddleLeft, paddleRight, paddleUp)
+       // console.log(this.player.html.style)
 
         let ballRight = this.ball.x + 30
         let ballLeft = this.ball.x
@@ -129,6 +138,87 @@ function Game(){
                 this.ball.changeDir('UR')
             }
         }
+
+         if ((ballLeft  <= paddleRight) &&
+             (ballRight >= paddleLeft) &&
+             (ballUp    <= paddleDown) &&
+             (ballDown  >= paddleUp)){
+                console.log("hay colision")
+                /*if(ballLeft <= paddleRight){
+                    console.log("por la derecha")
+                    if (this.ball.dir === `DL`) {
+                        this.ball.dir = 'DR'
+                    } else if (this.ball.dir === 'UL') {
+                        this.ball.dir = 'UR'
+                    }
+                }*/
+
+                if (this.ball.dir === `DL`){
+                    if (ballLeft <= paddleRight){
+                        this.ball.dir = 'DR'
+                    } else if (ballDown >= paddleUp){
+                        this.ball.dir = 'UL'
+                    }
+                }
+
+                if (this.ball.dir === `DR`){
+                    if (ballRight >= paddleLeft){
+                        this.ball.dir = 'DL'
+                    } else if (ballDown >= paddleUp){
+                        this.ball.dir = 'UR'
+                    }
+                }
+
+                if (this.ball.dir === 'UL') {
+                    if (ballUp <= paddleDown) {
+                        this.ball.dir = 'DL'
+                    } else if (ballLeft <= paddleRight) {
+                        this.ball.dir = 'UR'
+                    }
+                }
+
+                if (this.ball.dir === 'UR') {
+                    if (ballUp <= paddleDown) {
+                        this.ball.dir = 'DR'
+                    } else if (ballRight >= paddleLeft) {
+                        this.ball.dir = 'UL'
+                    }
+                }
+
+
+
+
+                /*if(ballRight >= paddleLeft){
+                    console.log("por la izquierda")
+                    if (this.ball.dir === `DR`) {
+                        this.ball.dir = 'DL'
+                    } else if (this.ball.dir === 'UR') {
+                        this.ball.dir = 'UL'
+                    }
+                }
+
+                if(ballDown >= paddleUp){
+                    console.log("por arriba")
+                    if(this.ball.dir === `DR`){
+                        this.ball.dir = 'UR'
+                    } else if(this.ball.dir === 'DL'){
+                        this.ball.dir = 'UL'
+                    }
+                }
+
+                if (ballUp <= paddleDown){
+                    console.log("por abajo")
+                    if (this.ball.dir === `UR`) {
+                        this.ball.dir = 'DR'
+                    } else if (this.ball.dir === 'UL') {
+                        this.ball.dir = 'DL'
+                    }
+                }*/
+            }
+
+       /* if ((ballDown === paddleUp || ballUp === paddleDown ) && ballLeft <= paddleRight && ballRight >= paddleLeft) {
+            console.log('hay colisión')
+        }*/
     }
 
     this.startGame = function() {
@@ -159,15 +249,24 @@ function Paddle(){
     this.height = 20
     this.width  = 150
     this.html   = null
+    this.left = 0
+    this.top = 0
 
-    this.createPaddle = function(classP, width){
+    this.createPaddle = function(classP, width, height){
         this.html     = document.createElement('div')
         this.html.id  = `${classP}-paddle`
         this.html.classList.add('paddle')
         this.html.style.height = this.height + 'px'
         this.html.style.width  = this.width + 'px'
+        this.left = width
         this.html.style.left = ((width / 2) - (this.width / 2)) + 'px'
+        this.top = height * 0.5
+        this.html.style.top = this.top + 'px'
+    }
 
+    this.updateMove = function(left){
+        this.left = left
+        this.html.style.left = this.left + 'px'
     }
 }
 
@@ -190,7 +289,7 @@ function Ball(){
 
 
     this.move = function(){
-        console.log()
+        
         switch(self.dir){
             case 'UR':
                 self.x += self.step
